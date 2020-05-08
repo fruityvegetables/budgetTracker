e856dd153270cf05e97fd54a2391f8257d9855b2
fruityvegetables <gg.troyer@gmail.com>
2020-05-08 17:03:28 -0500

    moved manifest & trying first service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,14 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js"];
+const FILES_TO_CACHE = [
+  "/", 
+  "/index.html", 
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "../models/transaction.js",
+  "../routes/api.js",
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
